138c7c08b1898e5f2f260b9dba8b40680b5eb98c
mattssru <83696563+mattssru@users.noreply.github.com>
2024-08-31 16:25:32 +0700

    gold api

--- pages/api/goldprice.js
+++ pages/api/goldprice.js
@@ -9,6 +9,6 @@ export default async function handler(req, res) {
     res.status(200).json(response.data);
   } catch (error) {
     console.log("error", error);
-    res.status(500).json({ error: "Failed to fetch gold price" });
+    res.status(500).json({ error: `Failed to fetch gold price ${error}` });
   }
 }
