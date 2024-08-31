91a46b976ad0adbc3ebccc0151228155f9e85018
mattssru <83696563+mattssru@users.noreply.github.com>
2024-08-31 16:01:05 +0700

    gold api

--- pages/api/goldprice.js
+++ pages/api/goldprice.js
@@ -2,17 +2,21 @@ import axios from "axios";
 
 export default async function handler(req, res) {
   try {
+    console.log("Fetching gold price from external API...");
     const response = await axios.get(
       "https://traderider.com/gold/wp-content/plugins/traderider-fx/api-gold.php?start_date=&end_date="
     );
 
+    console.log("Data received:", response.data);
+
     if (response.data && response.data.items) {
       res.status(200).json(response.data);
     } else {
-      res.status(404).json({ error: "Data not found" });
+      console.error("Data format error:", response.data);
+      res.status(404).json({ error: "Data not found or format incorrect" });
     }
   } catch (error) {
-    console.error("Error fetching gold price:", error.message);
+    console.error("Error fetching gold price from API:", error.message);
     res.status(500).json({ error: "Failed to fetch gold price" });
   }
 }
